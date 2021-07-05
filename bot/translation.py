0a05def3a7cd6e49f1cfd7ed88a08bfd24548314
AghinKhalidCP <86180193+AghinKhalidCP@users.noreply.github.com>
2021-07-05 13:39:36 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>I'm Artuk Bey of ADBZ Group....🥰
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I'm working under the Commands of @Sultan_AladdinKeyqubad & @adbz_turkish_bot......
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+Join our Group:
+@Turkish_Islamic_Media
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Turkish_Islamic_Media
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Sultan_AladdinKeyqubad">AK</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/AghinKhalidCP/Autofilterv2">Click Me</a></i>
 """
